5ff7a12ab98adc10022fa1efeeffcb6a860a18e9
jaeyonging <wodyd1318@naver.com>
2024-10-12 18:48:30 +0900

    design fix

--- src/routes/Map.tsx
+++ src/routes/Map.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react'
 import { NaverMap } from '../components/NaverMap'
-import { FaAngleDoubleUp } from "react-icons/fa";
+import { FaAngleDoubleUp } from 'react-icons/fa';
 
 const Map = () => {
     const [longtitude, setLongtitude] = useState(37.5532054)
     const [latitude, setLatitude] = useState(127.0107185)
+    const [isPopupOpen, setPopUpOpen] = useState(true)
+
+    const handleMapClick = () => {
+        setPopUpOpen(!isPopupOpen);
+    };
+
     return (
         <>
-            <NaverMap longtitude={longtitude} latitude={latitude} />
-            <div className='h-[40vh] w-[100vw] absolute rounded-[50px] p-4 bottom-0 flex bg-[white]'>
-                hi
+            <div onClick={handleMapClick}>
+                <NaverMap longtitude={longtitude} latitude={latitude} />
             </div>
+            {isPopupOpen ? (
+                <div
+                    className={`h-[40vh] w-[100vw] absolute rounded-[50px] p-4 bottom-0 flex bg-[white] transition-transform duration-500 ease-in-out ${isPopupOpen ? 'translate-y-0' : 'translate-y-[40vh]'
+                        }`}
+                >
+                    레스토랑 1
+                </div>
+            ) : (
+                <div className="absolute bottom-[8vh] left-1/2 transform -translate-x-1/2 transition-transform duration-500 ease-in-out animate-bounce" onClick={handleMapClick}>
+                    <FaAngleDoubleUp size={50} />
+                </div>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default Map
+export default Map;
